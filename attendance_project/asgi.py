f8a8900b10308e1f8d3a843607068f4d11bc96a1
Omar Khayam <omarkhayam4917@gmail.com>
2026-05-16 11:31:18 +0600

    Auto-create superuser on app startup

--- attendance_project/asgi.py
+++ attendance_project/asgi.py
@@ -14,3 +14,11 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance_project.settings')
 
 application = get_asgi_application()
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+except Exception as e:
+    print("Error creating superuser:", e)
--- attendance_project/wsgi.py
+++ attendance_project/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'attendance_project.settings')
 
 application = get_wsgi_application()
+
+try:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+except Exception as e:
+    print("Error creating superuser:", e)
