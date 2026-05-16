d8fcf2e3eb286c29a27d756775b53c6b0ace3c9c
Omar Khayam <omarkhayam4917@gmail.com>
2026-05-16 12:01:39 +0600

    Add redirect from /admin to root

--- attendance_project/urls.py
+++ attendance_project/urls.py
@@ -5,7 +5,11 @@ from django.urls import path, include, re_path
 from django.views.static import serve
 from attendance.views import attendance_dashboard_view, set_language_view
 
+from django.views.generic import RedirectView
+
 urlpatterns = [
+    path("admin/", RedirectView.as_view(url='/', permanent=False)),
+    path("admin", RedirectView.as_view(url='/', permanent=False)),
     path("attendance-dashboard/", attendance_dashboard_view),
     path("i18n/", include("django.conf.urls.i18n")),  # Language switch endpoint
     path("set-lang/<str:lang>/", set_language_view, name="set_language"),
